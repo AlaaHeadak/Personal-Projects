8e677dc9e839dfdd4349eb0143582fe8edc7290e
AlaaHeadak <alaa.headak96@gmail.com>
2022-10-21 17:15:58 +0200

    ARM Peripherals

---
+++ ARM_WorkPlace/NVIC Peripheral/src/ARM_MCAL/NVIC/NVIC_Configuration.h
@@ -0,0 +1,15 @@
+/*
+ * NVIC_Configuration.h
+ *
+ *  Created on: Oct 21, 2022
+ *      Author: LENOVO
+ */
+
+#ifndef NVIC_NVIC_CONFIGURATION_H_
+#define NVIC_NVIC_CONFIGURATION_H_
+
+
+
+
+
+#endif /* NVIC_NVIC_CONFIGURATION_H_ */
