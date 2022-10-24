7722bda992649cd11e12e896c346e1e6831db715
AlaaHeadak <alaa.headak96@gmail.com>
2022-10-24 15:37:15 +0200

    ARM Peripherals

--- ARM_WorkPlace/NVIC_GPIO_RCC_EXTI/src/main.c
+++ ARM_WorkPlace/NVIC_GPIO_RCC_EXTI/src/main.c
@@ -9,19 +9,30 @@
 #include "ARM_MCAL/GPIO/GPIO_private.h"
 #include "ARM_MCAL/GPIO/GPIO_configurations.h"
 
+#include "ARM_MCAL/NVIC/NVIC_interface.h"
+
 #include "ARM_MCAL/EXTI/EXTI_interface.h"
 #include "ARM_MCAL/EXTI/EXTI_private.h"
 #include "ARM_MCAL/EXTI/EXTI_configuration.h"
 
 int main(void)
 {
+
+	/* controlling LED using DIP switch Turn(ON/OFF)*/
 	MRCC_voidInit();
 	MRCC_voidPeripheralEnable(AHB1,GPIOAEN);
-	MGPIO_voidSetPinDirection(GPIOA_PORT,PIN0,OUTPUT_PUSH_PULL,MEDIUM_SPEED);
+	MRCC_voidPeripheralEnable(APB2,SYSCFGEN);
+
+	MGPIO_voidSetPinDirection(GPIOA_PORT,PIN1,OUTPUT_PUSH_PULL,MEDIUM_SPEED);
+	MGPIO_voidSetPinValue(GPIOA_PORT,PIN1,HIGH);
+
+	MGPIO_voidSetPinDirection(GPIOA_PORT,PIN0,INPUT_PULL_UP,MEDIUM_SPEED);
 	MGPIO_voidSetPinValue(GPIOA_PORT,PIN0,HIGH);
 
 
+	MNVIC_VoidEnablePeripheral(ETI0);
 
+	MEXTI_voidConfig(EXTI0,GPIOA_PORT,FALLING_EDGE);
 
 	while(1)
 	{
