a5e3e50760043266ccd644502564d02fb64361ff
AlaaHeadak <alaa.headak96@gmail.com>
2022-10-25 17:38:24 +0200

    Python Projects

---
+++ Python_Projects/Quiz Project using OOP/main.py
@@ -0,0 +1,19 @@
+from Question_model import Question
+from data import question_data
+from quiz_begin import QuizBrain
+
+
+question_bank=[]
+for question in question_data:
+    question_text=question["question"]
+    question_answer=question["correct_answer"]
+    new_question=Question(question_text,question_answer)
+    question_bank.append(new_question)
+
+quiz=QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("Quiz completed")
+print(f"Final score: {quiz.score}/{quiz.question_number}")
